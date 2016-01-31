87b11489b93566400c39dc010325552b7e31a484
乔福 <qiaofu@taobao.com>
2016-01-31 20:20:10 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,19 @@ var gulp = require('gulp');
 var onehtml = require('gulp-onehtml');
 
 gulp.task('default', function () {
-    gulp.src('index.dev.html')
+    gulp.src([
+        'index.dev.html',
+        'index-tingting.dev.html'
+    ])
         .pipe(onehtml({
             perfix: 'perfix-',
             suffix: '-deploy',
             minify: true,
             base64: [  // 转换base64需要手工配置要转换的图片地址
                 './assets/images/cover.jpg',
-                './assets/images/map01.jpg'
+                './assets/images/pic08.jpg',
+                './assets/images/map01.jpg',
+                './assets/images/map02.jpg'
             ]
         }))
         .pipe(gulp.dest('./'))
